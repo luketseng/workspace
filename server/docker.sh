f0c1c47db04827678873d752ce97f0b2b7f71290
luke <luke_tseng@compalbn.com>
2018-08-29 10:00:26 +0800

    server: docker.sh: add boardfarm4 setting

--- server/docker.sh
+++ server/docker.sh
@@ -12,12 +12,16 @@ cd ~/cbn/boardfarm
 #set -x; IFACE=eth1 create_container_eth1_static wan-shared 172.16.1.232/24 172.16.1.1; set +x
 
 ## for boardfarm3 172.16.1.xx
-./deploy-boardfarm-nodes.sh eth0 101 102
-set -x; IFACE=eth1 create_container_eth1_static wan-shared 172.16.1.220/24 172.16.1.1; set +x
+#./deploy-boardfarm-nodes.sh eth0 101 102
+#set -x; IFACE=eth1 create_container_eth1_static wan-shared 172.16.1.220/24 172.16.1.1; set +x
+
+## for boardfarm4 172.16.1.246
+#./deploy-boardfarm-nodes.sh eth0 101 102
+#set -x; IFACE=eth1 create_container_eth1_static wan-shared 172.16.1.247/24 172.16.1.1; set +x
 
 ## docker set ip netmask gateway by manual
 #docker exec -it bft-node-eth1-wan-shared /bin/bash
-## docker for boardfarm ip:172.16.1.xxx (229, 232, 220)
+## docker for boardfarm ip:172.16.1.xxx (229, 232, 220, 247)
 #ifconfig eth1 172.16.1.xxx netmask 255.255.255.0 && route add default gw 172.16.1.1
 ## reboot start
 #@reboot echo cbn | sudo -S ~/cbn/boardfarm/docker.sh
