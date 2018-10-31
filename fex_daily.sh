513470f1bcb1e663d8a9f743956705cb56d1f737
luke <luke360351@gmail.com>
2018-10-31 14:35:02 +0800

    fex_daily.sh: add date_age to export

--- fex_daily.sh
+++ fex_daily.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
-./git/taifex_daily/mining_rpt.py -s $(date +%Y%m%d) $(date +%Y%m%d) 1 TX>TX_$(date +%Y%m%d)
-./git/taifex_daily/mining_rpt.py -s $(date +%Y%m%d) $(date +%Y%m%d) 1 MTX>MTX_$(date +%Y%m%d)
+today=$(date +%Y%m%d)
+date_age=$(date +%Y%m%d -d "${1:-0} days ago")
+./git/taifex_daily/mining_rpt.py -e $date_age $today TX 1  >  TX_$today
+./git/taifex_daily/mining_rpt.py -e $date_age $today  MTX 1 > MTX_$today
+sleep 1m
 find ~/git/taifex_daily/fut_rpt/ -mtime +15 -type f -name '*' -exec rm -rf {} \;
 find ~/git/taifex_daily/opt_rpt/ -mtime +15 -type f -name '*' -exec rm -rf {} \;
 find ~/ -mtime +0 -type f -name "*TX_*" -exec rm -rf {} \;
