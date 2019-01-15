749b9d3a9aa4490875753e1f5794041595a29108
luke <luke360351@gmail.com>
2019-01-15 14:34:52 +0800

    fex_daily.sh: add input $1 for run days ago

--- fex_daily.sh
+++ fex_daily.sh
@@ -7,16 +7,22 @@ today=$(date +%Y%m%d)
 #if [ "$1" = "" ]; then
 if [ ! $1 ]; then
     echo "you have not input a word!"
-    date_age=$(date +%Y%m%d -d "${1:-4} days ago")
+    date_age=$today
 else
     echo "the word you input is $1"
-    date_age=$1
+    date_age=$(date +%Y%m%d -d "${1} days ago")
 fi
 
 echo "get $date_age~$today fex"
-./git/taifex_daily/mining_rpt.py -e $date_age $today TX 1  >  TX_$today
-./git/taifex_daily/mining_rpt.py -e $date_age $today  MTX 1 > MTX_$today
+./git/taifex_daily/mining_rpt.py -e $date_age $today TX 1 > TX_$today
+./git/taifex_daily/mining_rpt.py -e $date_age $today MTX 1 > MTX_$today
 sleep 1m
 find ~/git/taifex_daily/fut_rpt/ -mtime +15 -type f -name '*' -exec rm -rf {} \;
 find ~/git/taifex_daily/opt_rpt/ -mtime +15 -type f -name '*' -exec rm -rf {} \;
 find ~/ -mtime +0 -type f -name "*TX_*" -exec rm -rf {} \;
+
+: '
+# for crontab -e
+10 19 * * 1-4 ./git/workspace/fex_daily.sh
+10 19 * * 5 ./git/workspace/fex_daily.sh 4
+'
