7e08f1f46a00a9fdb2bd74998c57140fbcea5083
luke <luke360351@gmail.com>
2023-07-08 14:28:12 +0800

    [MOD] modify mac for new home pc

--- wakeupWinPC.sh
+++ wakeupWinPC.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wakeonlan -i 192.168.10.255 00:26:18:14:9B:1B
+wakeonlan -i 192.168.10.255 3C:7C:3F:20:22:DB
+#wakeonlan -i 192.168.10.255 FC:34:97:A2:F4:9C
+#wakeonlan -i 192.168.10.255 00:26:18:14:9B:1B
