fdf85250231f9ac6a56e58ecf9668ca9a49bea35
luke <luke360351@gmail.com>
2024-10-03 18:21:28 +0800

    [UPD] Update for .htaccess and fex_daily.sh

--- fex_daily.sh
+++ fex_daily.sh
@@ -24,7 +24,12 @@ find ~/public_html -mtime +0 -type f -name "*TX_*" -exec rm -rf {} \;
 
 : '
 # for crontab -e
-05 19 * * * ./git/taifex_daily/mining_rpt.py
-10 19 * * 1-4 ./git/workspace/fex_daily.sh
-10 19 * * 5 ./git/workspace/fex_daily.sh 4
+05 15,19 * * * ./git/taifex_daily/mining_rpt.py --upload-recover > log.txt
+45 15 * * 1-5 ./git/taifex_daily/get_data.py >> log.txt
+30 15,20 * * 1-4 ./git/workspace/fex_daily.sh
+30 15,20 * * 5 ./git/workspace/fex_daily.sh 4
+#create a hard link to file
+ln file hard-file
+luke@raspberrypi:~/git/workspace(master 1h13m)$ ln -v /opt/backup/FCT_DB.db
+./FCT_DB.db' => '/opt/backup/FCT_DB.db
 '
