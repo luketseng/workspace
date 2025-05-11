953b26ef48302c874468cd45f9efc776a2a70892
luke <luke360351@gmail.com>
2025-05-11 09:25:46 +0000

    [UPD] Update README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ this is my work place
 
 2018.08.07  update server folder
 2019.09.28  Integrate dir and file
+2025.05.11  Integrate dir and file
---
+++ shell/git_user_mail.sh
@@ -0,0 +1,4 @@
+git config --global user.email luke360351@gmail.com
+git config --global user.name luke
+git config --global user.email
+git config --global user.name
