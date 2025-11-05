83deb51a8250b1570d9d29051a6d2c8f3be6f2e0
luke <luke360351@gmail.com>
2025-11-05 20:29:48 +0800

    [UPD] update wakeupWinPC.sh

--- wakeupWinPC.sh
+++ wakeupWinPC.sh
@@ -10,28 +10,28 @@
 BROADCAST_IP="192.168.10.255"
 
 # Configure the MAC address of each device
-DEVICE1_MAC="3C:7C:3F:20:22:DB" # Home
+DEVICE1_MAC="24:4B:FE:58:EA:B1" # Home
 DEVICE2_MAC="00:E0:4C:94:AB:0D" # USB HUB
-DEVICE3_MAC="74:56:3C:40:DE:33" # Home_borrow_MB
+DEVICE3_MAC="" # TBD
 
 case "$1" in
   1)
-    echo "Waking up Home_tmp..."
+    echo "Waking up Home..."
     wakeonlan -i "$BROADCAST_IP" "$DEVICE1_MAC"
     ;;
   2)
-    echo "Waking up Home..."
+    echo "Waking up USB HUB..."
     wakeonlan -i "$BROADCAST_IP" "$DEVICE2_MAC"
     ;;
   3)
-    echo "Waking up USB HUB..."
+    echo "Waking up TBD device..."
     wakeonlan -i "$BROADCAST_IP" "$DEVICE3_MAC"
     ;;
   *)
     echo "Usage: $0 {1|2|3}"
     echo "  1 - Home"
     echo "  2 - USB HUB"
-    echo "  3 - Home_borrow_MB"
+    echo "  3 - TBD"
     exit 1
     ;;
 esac
